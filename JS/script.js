64aa18d30516b521a3e7766b352d034923dd03b2
Flo2503 <bruel.florent@gmail.com>
2023-08-16 17:13:03 +0200

    add method generating number between 1 and 6. Added the value to the player one's score

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,7 @@
 const canvas = document.getElementById('dice');
 const ctx = canvas.getContext('2d');
+let scorePlayerOne = 0;
+
 
 // Creta new image
 const image = new Image();
@@ -18,3 +20,21 @@ function resetScores() {
         score.textContent = 'O';
     });
 }
+
+// Roll dice and add value to player's score
+document.addEventListener('DOMContentLoaded', function() {
+    const rollButton = document.getElementById('rollButton');
+    const resultElement = document.getElementById('diceResult');
+
+    rollButton.addEventListener('click', function() {
+        const randomNumber = Math.floor(Math.random() * 6) + 1;
+
+        scorePlayerOne += randomNumber;
+        resultElement.textContent = randomNumber;
+
+        const activePlayerScoreElement = document.querySelector('.score-player-one');
+        activePlayerScoreElement.textContent = scorePlayerOne;
+    });
+});
+
+
