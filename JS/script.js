64e8b4113fc036ff38a3c9ff6c90e06d13dec808
Flo2503 <bruel.florent@gmail.com>
2023-08-16 14:01:22 +0200

    add default image in the canvas, fnished font #1

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+const canvas = document.getElementById('dice');
+const ctx = canvas.getContext('2d');
+
+// Creta new image
+const image = new Image();
+image.src = "./Images/default_dice.png";
+
+// Loading image
+image.onload = () => {
+  // draw image
+  ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+};
