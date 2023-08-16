2cc5245de63f31464a0185b23a4bbeea8e33149c
Flo2503 <bruel.florent@gmail.com>
2023-08-16 15:30:09 +0200

    add method reseting game FIXES#2

--- JS/script.js
+++ JS/script.js
@@ -10,3 +10,11 @@ image.onload = () => {
   // draw image
   ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
 };
+
+// Reset scores clicking NEW GAME BUTTON
+function resetScores() {
+    const scores = document.querySelectorAll('.score');
+    scores.forEach(score => {
+        score.textContent = 'O';
+    });
+}
