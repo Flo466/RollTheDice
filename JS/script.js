e2701010e892be9323039c607dfc20e0aa3552ef
Flo2503 <bruel.florent@gmail.com>
2023-08-18 14:55:44 +0200

    add methods to save current score of each player

--- JS/script.js
+++ JS/script.js
@@ -3,6 +3,9 @@ const ctx = canvas.getContext('2d');
 let activePlayer = 1;
 let playerOneScore = 0;
 let playerTwoScore = 0;
+let playerOneCurrentScore = 0;
+let playerTwoCurrentScore = 0;
+
 
 // Creta new image
 const image = new Image();
@@ -19,6 +22,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const rollButton = document.getElementById('rollButton');
     const newGameButton = document.getElementById('newGameButton');
     const diceResult = document.getElementById('diceResult');
+    const holdButton = document.getElementById('hold-btn');
 
     function switchPlayers(currentPlayer) {
         return (currentPlayer === 1) ? 2 : 1;
@@ -30,6 +34,21 @@ document.addEventListener('DOMContentLoaded', function() {
             score.textContent = '0';
         });
     }
+
+    function updateScores() {
+        const playerOneScoreElement = document.querySelector('.score-player-one');
+        const playerTwoScoreElement = document.querySelector('.score-player-two');
+
+        playerOneScoreElement.textContent = playerOneScore;
+        playerTwoScoreElement.textContent = playerTwoScore;
+
+        const playerOneCurrentScoreElement = document.querySelector('.crnt-score-player-One');
+        const playerTwoCurrentScoreElement = document.querySelector('.crnt-score-player-Two');
+
+        playerOneCurrentScoreElement.textContent = playerOneCurrentScore;
+        playerTwoCurrentScoreElement.textContent = playerTwoCurrentScore;
+    }
+
 
     // Roll dice, generate random number, add it to active player's score
     rollButton.addEventListener('click', function() {
@@ -58,6 +77,21 @@ document.addEventListener('DOMContentLoaded', function() {
         activePlayerScoreElement.textContent = (activePlayer === 1) ? playerOneScore : playerTwoScore;
     });
 
+    holdButton.addEventListener('click', function() {
+        if (activePlayer === 1) {
+            playerOneCurrentScore += playerOneScore;
+            playerOneScore = 0;
+            resetScore('.score-player-one')
+        } else {
+            playerTwoCurrentScore += playerTwoScore;
+            playerTwoScore = 0;
+            resetScore('.score-player-two')
+        }
+
+        activePlayer = switchPlayers(activePlayer);
+        updateScores();
+    });
+
     // Reset game
     newGameButton.addEventListener('click', function() {
         playerOneScore = 0;
@@ -65,3 +99,5 @@ document.addEventListener('DOMContentLoaded', function() {
         resetScore('.score');
     });
 });
+
+    
